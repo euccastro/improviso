289812f459540eb452d48e8f36fb150776738bb9
Estevo U. C. Castro <euccastro@gmail.com>
2017-06-25 17:45:37 +0200

    comment out unused mixin handlers

--- src/cljs/improviso/gl.cljs
+++ src/cljs/improviso/gl.cljs
@@ -7,49 +7,53 @@
   [(.-innerWidth js/window)
    (.-innerHeight js/window)])
 
-(rum/defc canvas < (rum/local (window-size) ::window-size)
-                   {:init (fn [state props]
-                            (println "init" state props)
-                            state)
-                    :will-mount (fn [state]
-                                  (println "will mount" state)
-                                  state)
-                    :before-render (fn [state]
-                                     (println "before render" state)
-                                     state)
-                    :wrap-render (fn [render]
-                                   (println "wrapping render")
-                                   (fn [& args]
-                                     (println "rendering" args)
-                                     (apply render args)))
-                    :did-mount (fn [{[{:keys [on-resize]}] :rum/args :as state}]
-                                 (println "did mount" state)
-                                 (comment (reset! (::window-size state)
-                                                  (window-size)))
-                                 (.addEventListener js/window
-                                                    "resize"
-                                                    (fn []
-                                                      (let [[w h] (window-size)]
-                                                        (println "resizing to" w h on-resize)
-                                                        (on-resize w h)
-                                                        (reset! (::window-size state)
-                                                                [w h]))))
-                                 state)
-                    :after-render (fn [state]
-                                    (println "after render" state)
-                                    state)
-                    :did-remount (fn [old-state state]
-                                   (println "did remount" old-state state)
-                                   state)
-                    :will-update (fn [state]
-                                   (println "will update" state)
-                                   state)
-                    :did-update (fn [state]
-                                  (println "did update" state)
-                                  state)
-                    :will-unmount (fn [state]
-                                    (println "will unmount" state)
-                                    state)}
+(rum/defc canvas <
+  (rum/local (window-size) ::window-size)
+  rum/static
+  {
+   ;; :init (fn [state props]
+   ;;         (println "init" state props)
+   ;;         state)
+   ;; :will-mount (fn [state]
+   ;;               (println "will mount" state)
+   ;;               state)
+   ;; :before-render (fn [state]
+   ;;                  (println "before render" state)
+   ;;                  state)
+   ;; :wrap-render (fn [render]
+   ;;                (println "wrapping render")
+   ;;                (fn [& args]
+   ;;                  (println "rendering" args)
+   ;;                  (apply render args)))
+   :did-mount (fn [{[{:keys [on-resize]}] :rum/args :as state}]
+                (println "did mount" state)
+                (comment (reset! (::window-size state)
+                                 (window-size)))
+                (.addEventListener js/window
+                                   "resize"
+                                   (fn []
+                                     (let [[w h] (window-size)]
+                                       (println "resizing to" w h on-resize)
+                                       (on-resize w h)
+                                       (reset! (::window-size state)
+                                               [w h]))))
+                state)
+   ;; :after-render (fn [state]
+   ;;                 (println "after render" state)
+   ;;                 state)
+   ;; :did-remount (fn [old-state state]
+   ;;                (println "did remount" old-state state)
+   ;;                state)
+   ;; :will-update (fn [state]
+   ;;                (println "will update" state)
+   ;;                state)
+   ;; :did-update (fn [state]
+   ;;               (println "did update" state)
+   ;;               state)
+   ;; :will-unmount (fn [state]
+   ;;                 (println "will unmount" state)
+   ;;                 state)
+   }
   [handlers]
   [:canvas {:style {:width (.-innerWidth js/window)
                     :height (.-innerHeight js/window)
