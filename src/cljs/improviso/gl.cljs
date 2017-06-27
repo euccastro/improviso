924ef86512fafe6e537f6277cbf0b623d8db2113
Estevo U. C. Castro <euccastro@gmail.com>
2017-06-27 02:46:45 +0200

    cleanup: request new frame explicitly instead of using bogus local

--- src/cljs/improviso/gl.cljs
+++ src/cljs/improviso/gl.cljs
@@ -7,8 +7,17 @@
   [(.-innerWidth js/window)
    (.-innerHeight js/window)])
 
+;; extracted out for live coding
+(defn on-resize [{[{resize-handler :on-resize}] :rum/args
+                  react-comp :rum/react-component
+                  :as state}]
+  (let [[w h] (window-size)]
+    (println "resizing to.." w h)
+    (when resize-handler
+      (resize-handler w h (rum/dom-node state)))
+    (rum/request-render react-comp)))
+
 (rum/defc canvas <
-  (rum/local (window-size) ::window-size)
   rum/static
   {
    ;; :init (fn [state props]
@@ -17,38 +26,32 @@
    ;; :will-mount (fn [state]
    ;;               (println "will mount" state)
    ;;               state)
-   :before-render (fn [state]
-                    (println "before render")
-                    state)
+   ;; :before-render (fn [state]
+   ;;                  (println "before render")
+   ;;                  state)
    ;; :wrap-render (fn [render]
    ;;                (println "wrapping render")
    ;;                (fn [& args]
    ;;                  (println "rendering" args)
    ;;                  (apply render args)))
-   :did-mount (fn [{[{:keys [on-resize]}] :rum/args :as state}]
-                (let [on-resize (fn []
-                                  (let [[w h] (window-size)]
-                                    (println "resizing to" w h)
-                                    (reset! (::window-size state)
-                                            [w h])
-                                    (when on-resize
-                                      (on-resize w h (rum/dom-node state)))))]
+   :did-mount (fn [state]
+                (let [handler #(on-resize state)]
                   (.addEventListener js/window
                                      "resize"
-                                     on-resize)
-                  (assoc state ::on-resize on-resize)))
+                                     handler)
+                  (assoc state ::on-resize handler)))
    :after-render (fn [{[{:keys [after-render]}] :rum/args :as state}]
                    (when after-render (after-render (rum/dom-node state)))
                    state)
    ;; :did-remount (fn [old-state state]
    ;;                (println "did remount" old-state state)
    ;;                state)
-   :will-update (fn [state]
-                  (println "will update")
-                  state)
-   :did-update (fn [state]
-                 (println "did update")
-                 state)
+   ;; :will-update (fn [state]
+   ;;                (println "will update")
+   ;;                state)
+   ;; :did-update (fn [state]
+   ;;               (println "did update")
+   ;;               state)
    :will-unmount (fn [state]
                    (println "will unmount")
                    (when-let [on-resize (::on-resize state)]
@@ -66,5 +69,6 @@
               :padding "0"
               :margin "0"
               :border "0"
+              ;; magenta as sentinel color
               :background-color "#ff00ff"}}]))
 
